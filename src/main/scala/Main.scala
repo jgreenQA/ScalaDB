0880912fd966e03598229c43a6532cb0b9dc3737
jgreenQA <jack.green@qa.com>
2017-08-25 12:56:58 +0000

    Updated

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -20,7 +20,7 @@ object Main extends App {
     Await.result(dropFuture, Duration.Inf).andThen {
       case Success(_) => initialisePeople
       case Failure(error) =>
-        println("Dropping the table failed due to: " + error.getMessage)
+        println(s"Dropping the table failed due to: ${error.getMessage}")
         initialisePeople
     }
   }
@@ -31,46 +31,144 @@ object Main extends App {
     }
     Await.result(setupFuture, Duration.Inf).andThen {
       case Success(_) => runQuery
-      case Failure(error) => println("Initialising the table failed due to: " + error.getMessage)
+      case Failure(error) => println(s"Initialising the table failed due to: ${error.getMessage}")
     }
   }
 
   def runQuery = {
     val insertPeople = Future {
       val query = peopleTable ++= Seq(
-        (10, "Jack", "Wood", 36),
-        (20, "Tim", "Brown", 24)
+        (10, "Jack", "Wood", 36, "1 Test St"),
+        (20, "Tim", "Brown", 24, "2 Test St")
       )
       println(query.statements.head)
       db.run(query)
     }
     Await.result(insertPeople, Duration.Inf).andThen {
-      case Success(_) => updatePeople
-      case Failure(error) => println("Welp! Something went wrong! " + error.getMessage)
+      case Success(_) => updatePeople(1, "Jack", "Wood", 46, "3 Test St")
+      case Failure(error) => println(s"Inserting people failed due to: ${error.getMessage}")
     }
   }
 
   def listPeople = {
     val queryFuture = Future {
       db.run(peopleTable.result).map(_.foreach {
-        case (id, fName, lName, age) => println(s"$id $fName $lName $age")
+        case (id, fName, lName, age, address) => println(s"$id $fName $lName $age $address")
       })
     }
     Await.result(queryFuture, Duration.Inf).andThen {
-      case Success(_) => db.close()
-      case Failure(error) => println("Listing people failed due to: " + error.getMessage)
+      case Success(_) => //db.close()
+      case Failure(error) => println(s"Listing people failed due to: ${error.getMessage}")
     }
   }
 
-  def updatePeople = {
+  def updatePeople(searchId: Int, fname: String, lname: String, age: Int, address: String) = {
+    listPeople
+
     val updateFuture = Future {
-      val query = peopleTable.insertOrUpdate(1, "Jack", "Wood", 46)
+      val query = peopleTable.filter(_  .id === searchId).update(searchId, fname, lname, age, address)
       println(query.statements.head)
       db.run(query)
     }
     Await.result(updateFuture, Duration.Inf).andThen {
+      case Success(_) => deletePeople(1)
+      case Failure(error) => println(s"Updating person $searchId failed due to: ${error.getMessage}")
+    }
+  }
+
+  def deletePeople(searchId: Int) = {
+    listPeople
+
+    val deleteFuture = Future {
+      val query = peopleTable.filter(_.id === searchId).delete
+      println(query.statements.head)
+      db.run(query)
+    }
+    Await.result(deleteFuture, Duration.Inf).andThen {
+      case Success(_) => countPeople
+      case Failure(error) => println(s"Deleting person $searchId failed due to: ${error.getMessage}")
+    }
+  }
+
+  def searchPeople(searchId: Int) = {
+    listPeople
+
+    val queryFuture = Future {
+      db.run(peopleTable.result).map(_.foreach {
+        case (id, fName, lName, age, address) if id == searchId => println(s"$id $fName $lName $age $address")
+      })
+    }
+    Await.result(queryFuture, Duration.Inf).andThen {
       case Success(_) => listPeople
-      case Failure(error) => println("Welp! Something went wrong! " + error.getMessage)
+      case Failure(error) => println(s"Searching people failed due to: ${error.getMessage}")
+    }
+  }
+
+  def countPeople = {
+    listPeople
+
+    val countFuture = Future {
+      val query = peopleTable.countDistinct.result
+      println(query.statements.head)
+      db.run(query)
+    }
+    Await.result(countFuture, Duration.Inf).andThen {
+      case Success(res) =>
+        println(res)
+        avgAgePeople
+      case Failure(error) => println(s"Counting people failed due to: ${error.getMessage}")
+    }
+  }
+
+  def avgAgePeople = {
+    listPeople
+
+    val avgFuture = Future {
+      val query = peopleTable.map(_.age).avg.result
+      println(query.statements.head)
+      db.run(query)
+    }
+    Await.result(avgFuture, Duration.Inf).andThen {
+      case Success(res) =>
+        println(res)
+        commonFnamePeople
+      case Failure(error) => println(s"Selecting people failed due to: ${error.getMessage}")
+    }
+  }
+
+  def commonFnamePeople = {
+    listPeople
+
+    val commonFnameFuture = Future {
+      val query = peopleTable.groupBy(p => p.fName)
+        .map{ case (fname, group) => (fname, group.map(_.fName).countDistinct) }
+        .result
+
+      println(query.statements.head)
+      db.run(query)
+    }
+    Await.result(commonFnameFuture, Duration.Inf).andThen {
+      case Success(res) =>
+        println(res)
+        commonLnamePeople
+      case Failure(error) => println(s"Selecting people failed due to: ${error.getMessage}")
+    }
+  }
+
+  def commonLnamePeople = {
+    listPeople
+
+    val commonLnameFuture = Future {
+      val query = peopleTable.groupBy(p => p.lName)
+        .map{ case (lName, group) => (lName, group.map(_.lName).countDistinct) }
+        .result
+
+      println(query.statements.head)
+      db.run(query)
+    }
+    Await.result(commonLnameFuture, Duration.Inf).andThen {
+      case Success(res) => println(res)
+      case Failure(error) => println(s"Selecting people failed due to: ${error.getMessage}")
     }
   }
 }
